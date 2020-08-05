f09d714f71fc173bad769c173bbe195b9a571be6
소루 || soru_love <64895958+sorulove@users.noreply.github.com>
2020-08-05 15:20:33 +0900

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
   function showPopup() { window.open("popup_add.html", "합작원 신청", "width=400, height=300, left=100, top=50"); }
 
-  var userName = prompt("당신의 아이디는 무엇인가요?");
+  var userName = prompt("당신의 아이디는 무엇인가요?","게스트1");
 document.write ("<h2>안녕하세요!" + userName + "님!</h2>")
