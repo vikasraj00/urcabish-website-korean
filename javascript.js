f976eb78b7c9f00b4ce3e5c3b5c5be707cf6a3c5
acatman1 <64052961+acatman1@users.noreply.github.com>
2020-08-07 01:44:16 +0900

    폰으로 업뎃

--- javascript.js
+++ javascript.js
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
   function showPopup() { window.open("popup_add.html", "합작원 신청", "width=400, height=300, left=100, top=50"); }
 
   var userName = prompt("당신의 아이디는 무엇인가요?","guest");
 document.write ("<h2>안녕하세요!" + userName + "님!</h2>")
-=======
-  function showPopup() { window.open("popup_add.html", "합작원 신청", "width=400, height=300, left=100, top=50"); }
-
-  var userName = prompt("당신의 아이디는 무엇인가요?");
-document.write ("<h2>안녕하세요!" + userName + "님!</h2>")
->>>>>>> 2b15dd4720d69c3576e35857f173399422a73868
