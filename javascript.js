a43cf160184a0430d1118fbb94343bb7aa3744cd
acatman1 <justinjinwooklee@gmail.com>
2020-09-27 12:38:02 +0900

    ip 삭제

--- javascript.js
+++ javascript.js
@@ -1,12 +1,3 @@
 const nick = require('./nickname.json');
 
   function showPopup() { window.open("popup_add.html", "합작원 신청", "width=400, height=300, left=100, top=50"); }
-
-if (ip() == nick[userip.indexOf(ip())]
-   document.write ("<h2>안녕하세요!" + userName + "님!</h2>")
-document.write ("<h2>안녕하세요!" + userName + "님!</h2>")
-} else {
-  var userName = prompt("당신의 아이디는 무엇인가요?","guest");
-  nick.push({'userip': ip(), 'nickname': userName})
-document.write ("<h2>안녕하세요!" + userName + "님!</h2>")
-}
